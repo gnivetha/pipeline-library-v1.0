67b27095a5a33cb1858db48a9d6df4c9e72bba13
gnivetha <38122219+gnivetha@users.noreply.github.com>
2019-05-31 20:37:20 +0530

    comment some lines

--- vars/buildFile.groovy
+++ vars/buildFile.groovy
@@ -1,18 +1,18 @@
 #!/usr/bin/env groovy
 
-def call(def buildTool) {
+def call(def buildtype) {
 	
-	if( ${buildToll}==snap )
+	if( ${buildtype}==snap )
 	{
 		echo "snapsnot_build"
-		def mvnHome = tool "${buildTool}";
-	sh "${mvnHome}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
+//		def mvnHome = tool "${buildTool}";
+	sh "${datas.buildTool}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
 	}
 	else
 	{
 		echo "release_build"
-		def mvnHome = tool "${buildTool}";
-	sh "${mvnHome}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
+//		def mvnHome = tool "${buildTool}";
+	sh "${datas.buildTool}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
 	}
 }
 		
