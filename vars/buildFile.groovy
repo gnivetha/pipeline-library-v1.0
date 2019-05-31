d42a43f17ccde96b0df627e9fa1b85f9718762e3
gnivetha <38122219+gnivetha@users.noreply.github.com>
2019-05-31 20:30:00 +0530

    added if conditions

--- vars/buildFile.groovy
+++ vars/buildFile.groovy
@@ -1,8 +1,21 @@
 #!/usr/bin/env groovy
 
 def call(def buildTool) {
+	
+	if( ${buildToll}==snap )
+	{
+		echo "snapsnot_build"
 		def mvnHome = tool "${buildTool}";
 	sh "${mvnHome}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
+	}
+	else
+	{
+		echo "release_build"
+		def mvnHome = tool "${buildTool}";
+	sh "${mvnHome}/bin/mvn "+"${datas.mavenGoals}"+" -Dbuild.number=${BUILD_NUMBER}"
+	}
+}
+		
 }
 
 
