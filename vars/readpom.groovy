e692a645a0e901e220ced8ad18e5135a91dd7e92
gnivetha <38122219+gnivetha@users.noreply.github.com>
2019-06-19 19:57:10 +0530

    added new groovy file

---
+++ vars/readpom.groovy
@@ -0,0 +1,6 @@
+def pom = new XmlSlurper().parse("pom.xml")
+ 
+pom.dependencies.dependency.each { dependency ->
+        println "!path \${mavenRepo}/${dependency.groupId}/${dependency.artifact
+Id}/${dependency.version}/${dependency.artifactId}-${dependency.version}.jar"
+}
