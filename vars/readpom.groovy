ba088ef4d3a2bfee811512b2809f3cda19daab19
gnivetha <38122219+gnivetha@users.noreply.github.com>
2019-06-19 19:59:53 +0530

    added def function

--- vars/readpom.groovy
+++ vars/readpom.groovy
@@ -1,6 +1,9 @@
+def call(def readpom) {
+
 def pom = new XmlSlurper().parse("pom.xml")
  
 pom.dependencies.dependency.each { dependency ->
         println "!path \${mavenRepo}/${dependency.groupId}/${dependency.artifact
 Id}/${dependency.version}/${dependency.artifactId}-${dependency.version}.jar"
+}
 }
